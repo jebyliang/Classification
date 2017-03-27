c41c605b74a70bc8f576c4ee411ca520c0e3d6ce
Zhao Liang <jebyliang@ucla.edu>
2017-03-26 23:58:50 -0700

    Update methods_code.r

--- methods_code.r
+++ methods_code.r
@@ -2,10 +2,19 @@
 
 # 1. KNN
 
-> set.seed(123)
-> dim(iris)
-[1] 150   5
-> n <- sort(c(sample(1:50, 35), sample(51:100, 35), sample(101:150, 35)))
-> train_x <- iris[n,1:4]
-> train_y <- iris[n,5]
-> distance <- function(a, b) return(sqrt((a[1]-b[1])^2+(a[2]-b[2])^2+(a[3]-b[3])^2+(a[4]-b[4])^2))
+distance <- function(a, b) return(sqrt((a[1]-b[1])^2+(a[2]-b[2])^2+(a[3]-b[3])^2+(a[4]-b[4])^2))
+
+library(dplyr)
+iris_knn <- function(x, k){
+  n <- sort(sample(1:dim(iris)[1], .7*dim(iris)[1]))
+  train_x <- as.matrix(iris[n,1:4])
+  train_y <- data.frame(iris[n,5])
+  colnames(train_y) <- names(iris)[5]
+  train_y$dis <- NULL
+  for (i in dim(train_x)[1]){
+    train_y$dis[i] <- distance(as.vector(x), as.vector(train_x[i,]))
+  }
+  train_y <- arrange(train_y, dis)[1:k,] %>% group_by(Species) %>% count(Species) %>% arrange(desc(n))
+  return(as.character(train_y$Species[1]))
+}
+
